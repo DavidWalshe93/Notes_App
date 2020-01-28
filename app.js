a761fffc5280461f5a7d5ac31a8e94d3531d7536
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-01-28 22:11:46 +0000

    Added chalk lib and changed the way colour is displayed in the console.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require("./notes");
+const log = console.log;
 
 console.log(getNotes());
 
 console.log(validator.isEmail("test.hello@example.com"));
 console.log(validator.isURL("https://hello.com"));
+
+log(chalk.green("Success"));
+log(chalk.bold.bgCyan("Success"));

72d917adf0fa4a65de21392dab5a514b00c6265c
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-01-28 22:02:26 +0000

    Added a npm package "validator" and validated a mock email and URL

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const validator = require('validator');
 const getNotes = require("./notes");
 
 console.log(getNotes());
+
+console.log(validator.isEmail("test.hello@example.com"));
+console.log(validator.isURL("https://hello.com"));

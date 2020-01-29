6b01c1f82940dfe6ec1a8383c6455652bd3f6a0f
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-01-29 21:26:49 +0000

    Added some colour notifications for note removal success

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const chalk = require('chalk');
 
 const getNotes = function () {
     return "Your notes..."
@@ -23,11 +24,15 @@ const addNote = function (title, body) {
 };
 
 const removeNote = function (title) {
-    console.log("Removing note " + title);
     const notes = loadNotes();
     const notesToKeep = notes.filter( function (note) {
         return note.title !== title
     });
+    if( notes.length > notesToKeep.length ) {
+        console.log(chalk.green.inverse("Note removed"));
+    } else {
+        console.log(chalk.red.inverse("No note found"))
+    }
     saveNotes(notesToKeep);
 };
 

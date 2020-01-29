9fff9fb6feae859dd26241fa2b187cd9e49febaf
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-01-29 20:36:33 +0000

    Added advanced yargs command line parsing

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
-const validator = require('validator');
-const chalk = require('chalk');
-const getNotes = require("./notes");
+// const validator = require('validator');
+// const chalk = require('chalk');
+const yargs = require("yargs");
+// const getNotes = require("./notes");
+
+// Customize yargs version
+yargs.version("1.1.0");
+
+yargs.command({
+    command: "add",
+    describe: "Add a new note",
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: "Body of note",
+            demandOption: true,
+            type: "string"
+        }
+    },
+    handler: function(argv) {
+        console.log("Title: " + argv.title + "\n" + argv.body)
+    }
+});
+
+yargs.command({
+    command: "remove",
+    describe: "Remove a note",
+    handler: function () {
+        console.log("Removing a note")
+    }
+});
 
 
-const command = process.argv[2];
+yargs.command({
+    command: "list",
+    describe: "List all notes",
+    handler: function () {
+        console.log("Listing notes")
+    }
+});
 
-if (command === 'add') {
-    console.log("Adding note")
-} else if (command === 'remove') {
-    console.log("Removing note")
-}
+yargs.command({
+    command: "read",
+    describe: "Reading notes",
+    handler: function () {
+        console.log("Reading notes")
+    }
+});
+
+
+yargs.parse();
+
+
